eea7d6de52f8a5f8fe51e6b1cd963913b048a279
Ben Rehmann <brehmann@me.com>
2024-11-06 18:25:16 -0600

    basic parallax

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import hobbiton from './images/hobbiton.jpg'
 import stingrayIcon from './images/DGreenIcon.png'
 import oneRing from './images/oneRing.png'
-
+import mordor from './images/mordor.png'
 
 function App() {
 
@@ -23,34 +23,23 @@ function App() {
     'http://res.cloudinary.com/dzh1qe1zp/image/upload/v1704915035/squareEleven.jpg'
   ];
 
+  const appContainerRef = useRef(null)
+  const scenicOptionsRef = useRef(null)
   const bannerRef = useRef(null)
   const introRef = useRef(null)
   const stingrayRef = useRef(null)
 
-
   const parallaxElements = [
-    { ref: bannerRef, speed: 0.185, direction: 'down' },
+    { ref: bannerRef, speed: 0.21, direction: 'down' },
     { ref: introRef, speed: 0.3, direction: 'up' },
-    { ref: stingrayRef, speed: 0.5, direction: 'up' },
+    { ref: stingrayRef, speed: 0.75, direction: 'up' },
   ];
-
-  const scenicContainerRef = useRef(null); // New ref for scenic options
   const [opacity, setOpacity] = useState(0); // State to track opacity
-  const oneRingRef = useRef(null);
-  const [rotation, setRotation] = useState(0);
-
-  const [pageScroll, setPageScroll] = useState(true)
-  const [scrollEnabled, setScrollEnabled] = useState(false)
-
-
-
-
-
 
 
 
   const handleParallax = () => {
-    const scrollPosition = window.scrollY
+    const scrollPosition = appContainerRef.current.scrollTop
     // Loop through each element and apply the parallax effect
     parallaxElements.forEach(({ ref, speed, direction }) => {
       if (ref.current) {
@@ -60,122 +49,73 @@ function App() {
       }
     })
     // Fade effect for oneRing based on scroll position
-    if (scenicContainerRef.current) {
-      const scenicOptionsRect = scenicContainerRef.current.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
-      // Calculate the center of the `scenicOptions` div
-      const scenicOptionsCenter = (scenicOptionsRect.top + scenicOptionsRect.bottom) / 2;
-      // Calculate the middle of the viewport
-      const windowCenter = windowHeight / 2;
-      // Calculate opacity based on how close the center of the div is to the middle of the viewport
-      const distanceFromCenter = Math.abs(windowCenter - scenicOptionsCenter);
-      const newOpacity = Math.min(Math.max(1 - distanceFromCenter / windowHeight, 0), 1);
-      setOpacity(newOpacity);
+    if (scenicOptionsRef.current) {
+      const scenicOptionsRect = scenicOptionsRef.current.getBoundingClientRect()
+      const containerHeight = appContainerRef.current.clientHeight
+      const scenicOptionsCenter = (scenicOptionsRect.top + scenicOptionsRect.bottom) / 2
+      const containerCenter = containerHeight / 2
+      const distanceFromCenter = Math.abs(containerCenter - scenicOptionsCenter)
+      const newOpacity = Math.min(Math.max(1 - distanceFromCenter / containerHeight, 0), 1)
+      setOpacity(newOpacity)
     }
   }
-
-  const preventPageScroll = (event) => {
-    if (!pageScroll) {
-      event.preventDefault()
-    }
-  }
-
-
   useEffect(() => {
-    // Add event listener to handle scroll based on pageScroll state
-    const scrollListener = pageScroll ? handleParallax : preventPageScroll;
-
-    window.addEventListener('scroll', scrollListener, { passive: false });
-
+    const appContainer = appContainerRef.current
+    // Event listener appContainer scroll
+    appContainer.addEventListener('scroll', handleParallax)
     // Cleanup event listener on unmount
     return () => {
-      window.removeEventListener('scroll', scrollListener);
+      appContainer.removeEventListener('scroll', handleParallax)
     }
-  }, [pageScroll]);
+  }, [parallaxElements])
 
-
-
-  // useEffect(() => {
-  //   // Add event listener to handle scroll
-  //   window.addEventListener('scroll', handleParallax)
-  //   // Cleanup event listener on unmount
-  //   return () => window.removeEventListener('scroll', handleParallax)
-  // }, [parallaxElements])
-
-
-  // ORIGINAL NORMAL PARALLAX EFFECT
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrollPosition = window.scrollY
-  //     // Loop through each element and apply the parallax effect
-  //     parallaxElements.forEach(({ ref, speed, direction }) => {
-  //       if (ref.current) {
-  //         // If the direction is 'up', we apply negative translation, otherwise, apply positive
-  //         const translation = scrollPosition * speed
-  //         ref.current.style.transform = `translateY(${direction === 'up' ? -translation : translation}px)`
-  //       }
-  //     })
-  //   }
-  //   // Add event listener to handle scroll
-  //   window.addEventListener('scroll', handleScroll)
-  //   // Cleanup event listener on unmount
-  //   return () => window.removeEventListener('scroll', handleScroll)
-  // }, [])
-
-
-
-  const containerStyle = {
-    overflow: scrollEnabled ? 'scroll' : 'hidden',
-  }
 
 
 
 
   return (
 
-    <div className="stingrayScenics">
+    <div className='appContainer' ref={appContainerRef}>
 
-      <div className='navigation'>
-        <h1 className='title'>Stingray Scenics</h1>
-        <div className='navOptions'>
-          <p>Home</p>
-          <p>Models</p>
-          <p>Terrain</p>
-          <p>Reviews</p>
-        </div>
-      </div>
+      <div className="stingrayScenics">
 
-      <img className='bannerImage' ref={bannerRef} src={hobbiton} alt='banner image' />
-
-      <div className='scrollContent'>
-        <img className='stingrayIcon' src={stingrayIcon} alt='brandIcon' ref={stingrayRef} />
-
-        <div className='introduction' ref={introRef}>
-          <h2>Introduction</h2>
-          <p>Hello! I'm Ben!</p>
-        </div>
-
-
-        {/* Scenic options */}
-        <div className='scenicContainer' style={containerStyle} ref={scenicContainerRef}>
-            <img className='oneRing' src={oneRing} alt='the one ring' style={{ opacity: opacity }} />
-          <div className='scenicOptions'>
-            <div className='scenicItem'><h1>Models</h1></div>
-            <div className='scenicItem'><h1>Paints</h1></div>
-            <div className='scenicItem'><h1>Terrain</h1></div>
-            <div className='scenicItem'><h1>About</h1></div>
+        <div className='navigation'>
+          <h1 className='title'>Stingray Scenics</h1>
+          <div className='navOptions'>
+            <p>Home</p>
+            <p>Models</p>
+            <p>Terrain</p>
+            <p>Reviews</p>
           </div>
         </div>
 
+        <img className='bannerImage' ref={bannerRef} src={hobbiton} alt='banner image' />
+
+        <div className='scrollContent'>
+          <img className='stingrayIcon' src={stingrayIcon} alt='brandIcon' ref={stingrayRef} />
+
+          <div className='introduction' ref={introRef}>
+            <h2>Introduction</h2>
+            <p>Hello! I'm Ben!</p>
+          </div>
+
+          {/* Scenic options */}
+            <img className='oneRing' src={oneRing} alt='the one ring' style={{ opacity: opacity }} />
+            <div className='scenicOptions' ref={scenicOptionsRef}>
+              <div className='scenicItem'><h1>Models</h1></div>
+              <div className='scenicItem'><h1>Paints</h1></div>
+              <div className='scenicItem'><h1>Terrain</h1></div>
+              <div className='scenicItem'><h1>About</h1></div>
+          </div>
+
+        </div>
+
+        <div className='copyright'>
+          <p>@barbed_stingray 2024</p>
+        </div>
 
 
       </div>
-
-      <div className='copyright'>
-        <p>@barbed_stingray 2024</p>
-      </div>
-
-
     </div>
 
   );
