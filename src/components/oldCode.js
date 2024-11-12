d5a95396d7dc25b22484db0c1e3c08ffe36da387
Ben Rehmann <brehmann@me.com>
2024-11-12 11:10:13 -0600

    parallax success

--- src/components/oldCode.js
+++ src/components/oldCode.js
@@ -1,33 +1,33 @@
-const handleParallax = () => {
-    const scrollPosition = appContainerRef.current.scrollTop
-    // Loop through each element and apply the parallax effect
-    parallaxElements.forEach(({ ref, speed, direction }) => {
-      if (ref.current) {
-        // If the direction is 'up', we apply negative translation, otherwise, apply positive
-        const translation = scrollPosition * speed
-        ref.current.style.transform = `translateY(${direction === 'up' ? -translation : translation}px)`
-      }
-    })
-    // Fade effect for oneRing based on scroll position
-    if (scenicOptionsRef.current) {
-      const scenicOptionsRect = scenicOptionsRef.current.getBoundingClientRect()
-      const containerHeight = appContainerRef.current.clientHeight
-      const scenicOptionsCenter = (scenicOptionsRect.top + scenicOptionsRect.bottom) / 2
-      const containerCenter = containerHeight / 2
-      const distanceFromCenter = Math.abs(containerCenter - scenicOptionsCenter)
-      const newOpacity = Math.min(Math.max(1 - distanceFromCenter / containerHeight, 0), 1)
-      setOpacity(newOpacity)
-    }
-  }
-  useEffect(() => {
-    const appContainer = appContainerRef.current
-    // Event listener appContainer scroll
-    appContainer.addEventListener('scroll', handleParallax)
-    // Cleanup event listener on unmount
-    return () => {
-      appContainer.removeEventListener('scroll', handleParallax)
-    }
-  }, [parallaxElements])
+// const handleParallax = () => {
+//     const scrollPosition = appContainerRef.current.scrollTop
+//     // Loop through each element and apply the parallax effect
+//     parallaxElements.forEach(({ ref, speed, direction }) => {
+//       if (ref.current) {
+//         // If the direction is 'up', we apply negative translation, otherwise, apply positive
+//         const translation = scrollPosition * speed
+//         ref.current.style.transform = `translateY(${direction === 'up' ? -translation : translation}px)`
+//       }
+//     })
+//     // Fade effect for oneRing based on scroll position
+//     if (scenicOptionsRef.current) {
+//       const scenicOptionsRect = scenicOptionsRef.current.getBoundingClientRect()
+//       const containerHeight = appContainerRef.current.clientHeight
+//       const scenicOptionsCenter = (scenicOptionsRect.top + scenicOptionsRect.bottom) / 2
+//       const containerCenter = containerHeight / 2
+//       const distanceFromCenter = Math.abs(containerCenter - scenicOptionsCenter)
+//       const newOpacity = Math.min(Math.max(1 - distanceFromCenter / containerHeight, 0), 1)
+//       setOpacity(newOpacity)
+//     }
+//   }
+//   useEffect(() => {
+//     const appContainer = appContainerRef.current
+//     // Event listener appContainer scroll
+//     appContainer.addEventListener('scroll', handleParallax)
+//     // Cleanup event listener on unmount
+//     return () => {
+//       appContainer.removeEventListener('scroll', handleParallax)
+//     }
+//   }, [parallaxElements])
 
 
 
@@ -89,3 +89,55 @@ const handleParallax = () => {
   //   // Cleanup event listener on unmount
   //   return () => window.removeEventListener('scroll', handleScroll)
   // }, [])
+
+
+
+
+
+
+  // parallax
+      // const clip1 = useTransform(scrollY, [0, SECTION_HEIGHT], [25, 0])
+    // const clip2 = useTransform(scrollY, [0, SECTION_HEIGHT], [75, 100])
+    // const featureClipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`
+
+
+        // parallax images
+        const refOne = useRef()
+        const { scrollYProgress } = useScroll({
+            target: refOne,
+            offset: ['-300px end', 'end -200px']
+        })
+    
+
+        const imageOneOpacity = useTransform(scrollYProgress, [0.6, 1], [1, 0])
+        const imageOneScale = useTransform(scrollYProgress, [0.7, 1], [1, 0.7])
+        const yOne = useTransform(scrollYProgress, [0, 1], [-200, 200])
+        // watch your start and end points
+        // could also just use the scale: imageOneScale on motion.element
+        // this just combines two properties
+        const transformOne = useMotionTemplate`translateY(${yOne}px) scale(${imageOneScale})`
+    
+        // console log motion values
+        // useMotionValueEvent(scrollYProgress, 'change', (latest) =>
+        //     console.log(latest)
+        // )
+
+        <div className='parallaxContainer'>
+        <div className='parallaxBox red'
+            style={{ transform: `translateY(0px)` }}
+        ></div>
+
+        <div className='parallaxBox blue'
+            style={{ transform: `translateY(-200px)` }}
+        ></div>
+
+    </div>
+
+        <motion.img 
+        style={{ opacity: imageOneOpacity, transform: transformOne }}
+        ref={refOne} 
+        className='parallaxImage' 
+        src={rivendellCharge}>
+        </motion.img>
+
+    
--- src/pages/miniGallery/Miniatures.jsx
+++ src/pages/miniGallery/Miniatures.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef, useMemo } from 'react';
-import { motion, useTransform, useScroll, useMotionTemplate } from 'framer-motion';
+import { motion, useTransform, useScroll, useMotionTemplate, useMotionValueEvent } from 'framer-motion';
 
 import './miniatures.css';
 
@@ -27,6 +27,32 @@ const Miniatures = () => {
     const clip2 = useTransform(scrollY, [0, SECTION_HEIGHT], [75, 100])
     const featureClipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`
 
+
+    // parallax images
+    const refOne = useRef()
+
+    const { scrollYProgress } = useScroll({
+        target: refOne,
+        offset: ['-300px end', 'end -200px']
+    })
+
+
+    const imageOneOpacity = useTransform(scrollYProgress, [0.6, 1], [1, 0])
+    const imageOneScale = useTransform(scrollYProgress, [0.7, 1], [1, 0.7])
+
+    
+    const yOne = useTransform(scrollYProgress, [0, 1], [-200, 200])
+    // watch your start and end points
+    // could also just use the scale: imageOneScale on motion.element
+    // this just combines two properties
+    const transformOne = useMotionTemplate`translateY(${yOne}px) scale(${imageOneScale})`
+
+    // console log motion values
+    useMotionValueEvent(scrollYProgress, 'change', (latest) =>
+        console.log(latest)
+    )
+
+
     return (
         <div className='miniatureGallery'>
             <div className='emptyElement' />
@@ -35,6 +61,7 @@ const Miniatures = () => {
             <div className='logoContainer'
                 style={{ height: `calc(${SECTION_HEIGHT}px + 100dvh)` }}
             >
+                {/* center image */}
                 <motion.div
                     style={{
                         opacity: featureOpacity,
@@ -42,10 +69,28 @@ const Miniatures = () => {
                         clipPath: featureClipPath,
                     }}
                     className='featureElement'>
+                </motion.div>
 
-                    </motion.div>
+                {/* parallax images THESE WOULD BE COMPONENTS */}
+                <div className='parallaxContainer'>
+                    <div className='parallaxBox red'
+                        style={{ transform: `translateY(0px)` }}
+                    ></div>
+
+                    <div className='parallaxBox blue'
+                        style={{ transform: `translateY(-200px)` }}
+                    ></div>
+
+                    <motion.img 
+                    style={{ opacity: imageOneOpacity, transform: transformOne }}
+                    ref={refOne} 
+                    className='parallaxImage' 
+                    src={rivendellCharge}>
+                    </motion.img>
+                </div>
+
             </div>
-            
+
 
             <div className='emptyElement' />
         </div>
