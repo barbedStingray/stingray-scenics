9942fc88c0ef275f85eb177b315ddd01726bc379
Ben Rehmann <brehmann@me.com>
2024-11-24 12:04:36 -0600

    boxes

--- src/pages/homePage/StingrayScenics.jsx
+++ src/pages/homePage/StingrayScenics.jsx
@@ -41,15 +41,10 @@ const StingrayScenics = () => {
     const yBannerUp = useTransform(mainContainer, [0, 0.15], [0, -20])
 
 
-    const xMove = useTransform(mainContainer, [0, 0.15], ['0%', '-100%'])
+    const xMove = useTransform(mainContainer, [0, 0.1], ['0%', '-100%'])
     const opacity = useTransform(mainContainer, [0, 0.1], [1, 0])
-    const xMoveA = useTransform(mainContainer, [0, 0.15], ['0%', '-100%'])
+    const xMoveA = useTransform(mainContainer, [0, 0.1], ['0%', '-100%'])
     const opacityA = useTransform(mainContainer, [0, 0.1], [0, 1])
-
-
-    const bigImageWidth = useTransform(mainContainer, [0, 0.1, 0.2], ['25%', '50%', '100%'])
-    const mediumImageWidth = useTransform(mainContainer, [0, 0.1, 0.2], ['25%', '50%', '0%'])
-    const smallImageWidth = useTransform(mainContainer, [0, 0.1], ['25%', '0%'])
 
 
     const targetRef = useRef()
@@ -57,7 +52,37 @@ const StingrayScenics = () => {
     console.log('in view', isModelSectionInView)
 
 
+    // Box manuvers 
+    const box1w = useTransform(mainContainer, [0, 0.1], ['35dvw', '25dvw'])
+    const box1h = useTransform(mainContainer, [0, 0.1], ['40dvh', '40dvh'])
+    const box1Left = useTransform(mainContainer, [0, 0.1], ['0dvhw', '0dvw'])
+    const box1Top = useTransform(mainContainer, [0, 0.1], ['0dvh', '0dvh'])
 
+    const box2w = useTransform(mainContainer, [0, 0.1], ['25dvw', '15dvw'])
+    const box2h = useTransform(mainContainer, [0, 0.1], ['10dvh', '30dvh'])
+    const box2Left = useTransform(mainContainer, [0, 0.1], ['35dvhw', '25dvw'])
+    const box2Top = useTransform(mainContainer, [0, 0.1], ['0dvh', '0dvh'])
+
+    const box3w = useTransform(mainContainer, [0, 0.1], ['25dvw', '15dvw'])
+    const box3h = useTransform(mainContainer, [0, 0.1], ['30dvh', '10dvh'])
+    const box3Left = useTransform(mainContainer, [0, 0.1], ['35dvhw', '25dvw'])
+    const box3Bottom = useTransform(mainContainer, [0, 0.1], ['0dvh', '0dvh'])
+
+     
+    const box4w = useTransform(mainContainer, [0, 0.1], ['35dvw', '55dvw'])
+    const box4h = useTransform(mainContainer, [0, 0.1], ['25dvh', '15dvh'])
+    const box4Left = useTransform(mainContainer, [0, 0.1], ['60dvhw', '40dvw'])
+    const box4Top = useTransform(mainContainer, [0, 0.1], ['0dvh', '0dvh'])
+
+
+    const box5w = useTransform(mainContainer, [0, 0.1], ['35dvw', '55dvw'])
+    const box5h = useTransform(mainContainer, [0, 0.1], ['15dvh', '25dvh'])
+    const box5Left = useTransform(mainContainer, [0, 0.1], ['60dvw', '40dvw'])
+    const box5Bottom = useTransform(mainContainer, [0, 0.1], ['0dvh', '0dvh'])
+
+
+    const smallO = useTransform(mainContainer, [0, 0.045], [1, 0])
+    const bigO = useTransform(mainContainer, [0.05, 0.1], [0, 1])
 
 
     const gradientColor = useTransform(mainContainer, [0, 1], [
@@ -83,20 +108,54 @@ const StingrayScenics = () => {
                         {/* <SectionTitle homeTitle='Stingray' /> */}
                         <h1>Stingray Scenics</h1>
                     </div>
+
+
                     <div className='displayImages'>
-                        <motion.div className='imageContainer' style={{ width: smallImageWidth }}>
-                            <motion.img className='heroImage' src={droids} style={{}} />
+
+                        <motion.div className='dImage1' style={{ 
+                            height: box1h, width: box1w, top: box1Top, left: box1Left
+                            }}>
+                            {/* <div className='box'></div> */}
+                            Box 1
                         </motion.div>
-                        <motion.div className='imageContainer' style={{ width: mediumImageWidth }}>
-                            <motion.img className='heroImage' src={trolls} style={{}} />
+
+                        <motion.div className='dImage2' style={{ 
+                            height: box2h, width: box2w, top: box2Top, left: box2Left
+                        }}>
+                            <motion.div className='smallDiv' style={{ opacity: smallO }}><p>Small</p></motion.div>
+                            <motion.div className='bigDiv' style={{ opacity: bigO }}><p>BIG</p></motion.div>
                         </motion.div>
-                        <motion.div className='imageContainer' style={{ width: bigImageWidth }}>
-                            <motion.img className='heroImage' src={pelennorFields} style={{}} />
+
+                        <motion.div className='dImage3' style={{ 
+                            height: box3h, width: box3w, bottom: box3Bottom, left: box3Left
+                        }}>
+                            {/* <div className='box'></div> */}
+                            <motion.div className='figureDiv' style={{ opacity: smallO }}><p>Figures</p></motion.div>
+
                         </motion.div>
-                        <motion.div className='imageContainer' style={{ width: smallImageWidth }}>
-                            <motion.img className='heroImage' src={imperials} style={{}} />
+
+                        <motion.div className='dImage4' style={{
+                            height: box4h, width: box4w, top: box4Top, left: box4Left
+                        }}>
+                            {/* <div className='box'></div> */}
+                            Box 4
+
                         </motion.div>
+
+                        <motion.div className='dImage5' style={{ 
+                            height: box5h, width: box5w, bottom: box5Bottom, left: box5Left
+                        }}>
+                            {/* <div className='box'></div> */}
+                            Box 5
+
+                        </motion.div>
+
                     </div>
+
+
+
+
+
                     <div className='bannerScroll'>
                         <div className='banner'>
                             <div className='tinySwap'>
