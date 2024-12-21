c4302b492f2e192de4648302bd3ab67c4faaf0cd
Ben Rehmann <brehmann@me.com>
2024-12-21 14:34:58 -0600

    lets get rid of scroll snap

--- src/pages/galleryPage/GalleryPage.jsx
+++ src/pages/galleryPage/GalleryPage.jsx
@@ -15,13 +15,23 @@ const GalleryPage = () => {
         console.log('mainY', latest)
     )
 
+    const lotrY = useTransform(galleryContainer, [0, 0.24262607040913417, 0.4995242626070409], ['100%', '0%', '0%'])
+    const lotrScale = useTransform(galleryContainer, [0.24262607040913417, 0.4995242626070409], [1, 0.8])
 
 
     return (
         <div className='galleryPage' ref={galleryContainerRef}>
             <div className='galleryScrollDiv'>
                 <div className='groupDisplay'>Welcome to Gallery</div>
-                <div className='groupDisplay'>Lord of the Rings Display</div>
+                <div className='groupDisplay'>
+                    {/* <div className='lotrWrapper'> */}
+                        <motion.div className='lotrDisplay'
+                            style={{ y: lotrY }}
+                        >
+                            <p>Lord of the Rings Display</p>
+                        </motion.div>
+                    {/* </div> */}
+                </div>
                 <div className='groupDisplay'>Star Wars Display</div>
                 <div className='groupDisplay'>Terrain Display</div>
                 <div className='groupDisplay'>?? Display</div>
