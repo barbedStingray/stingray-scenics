936f8463fda795673e23f50226f85d2ccdc1d9cf
Ben Rehmann <brehmann@me.com>
2025-01-07 16:33:51 -0600

    save day

--- src/pages/groupDisplay/GroupDisplay.jsx
+++ src/pages/groupDisplay/GroupDisplay.jsx
@@ -33,9 +33,11 @@ const GroupDisplay = () => {
                 </div>
 
                 {theFellowship.map((member, i) => (
-                    <div key={i} className='artImage'>
-                        <p>{member}</p>
-                    </div>
+                    <ArtImage key={i}>
+                        <div className='artImage'>
+                            <p>{member}</p>
+                        </div>
+                    </ArtImage>
                 ))}
 
             </div>
@@ -48,4 +50,18 @@ const GroupDisplay = () => {
 export default GroupDisplay
 
 
-const ArtImage = ({ children }) => 
+const ArtImage = ({ children }) => {
+    const ref = useRef()
+    const isInView = useInView(ref, { margin: '0px 0px -20% 0px ' })
+
+    return (
+        <motion.div
+            ref={ref}
+            initial={{ opacity: 0, y: 20 }}
+            animate={isInView ? { opacity: 0, y: 0 } : { opacity: 0, y: 20 }}
+            transition={{ duration: 0.5 }}
+        >
+            {children}
+        </motion.div>
+    )
+}
