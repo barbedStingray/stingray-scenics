33722b0952d2a641270f3196bc9edddcb914ebdf
Ben Rehmann <brehmann@me.com>
2025-01-20 19:33:06 -0600

    style decisions needed

--- src/pages/commissionPage/CommissionPage.jsx
+++ src/pages/commissionPage/CommissionPage.jsx
@@ -6,21 +6,26 @@ const CommissionPage = () => {
 
     const colorShade = '#140000ee'
 
-    const comissionRef = useRef()
-    const { scrollYProgress: comissionContainer } = useScroll({
-        container: comissionRef,
+    const commissionRef = useRef()
+    const { scrollYProgress: commissionContainer } = useScroll({
+        container: commissionRef,
         offset: ['start start', 'end end']
     })
     // console logging
-    useMotionValueEvent(comissionContainer, 'change', (latest) =>
+    useMotionValueEvent(commissionContainer, 'change', (latest) =>
         console.log('mainY', latest)
     )
 
+    const ringRotate = useTransform(commissionContainer, [0, 1], ['0deg', '90deg'])
+    const comSectionOne = useTransform(commissionContainer, [0, 0.18, 0.38, 0], [0, 1, 1, 0])
+
+
+
 
     // * use for portfolio - double gradient shift
-    const doubleGradientColor = useTransform(comissionContainer, [0, 1], [
-        `linear-gradient(90deg,#00fbff,#008225)`,
-        `linear-gradient(190deg,#008225,#0d00ff)`,
+    const doubleGradientColor = useTransform(commissionContainer, [0, 1], [
+        `linear-gradient(45deg,#001e00,#121e00)`,
+        `linear-gradient(120deg,#1e0000,#0a001e)`,
     ])
     const gradientStyle = useMotionTemplate`${doubleGradientColor}`
 
@@ -30,14 +35,17 @@ const CommissionPage = () => {
     return (
         <motion.div
             className='commissionPage'
-            ref={comissionRef}
-            // style={{ background: gradientStyle }}
+            ref={commissionRef}
+            style={{ background: gradientStyle }}
         >
             <div className='scrollCommission'>
-                <div className='ringBox'></div>
+                <motion.div className='ringBox' style={{ rotate: ringRotate }}></motion.div>
             </div>
+            <div className='comSection'><h1>Commission</h1><p>Scroll to Learn More</p></div>
+            <div className='comSection'><h1>Models</h1><p>Get Models</p></div>
+            <div className='comSection'><h1>Process</h1><p>What I do</p></div>
+            <div className='comSection'><h1>Shipping</h1><p>Shipped for free</p></div>
 
-            <div className='comSection'></div>
         </motion.div>
     )
 }
